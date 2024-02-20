41b835565fe1779599d7b5c312a619d6bf32703a
imheeseung <gmltmd519@gmail.com>
2024-02-20 21:40:58 +0900

    Feat: Show results with figlet

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import { getTailwindCss } from "./tailwindCss.js";
 import { checkCssCompatibility } from "./cssCompatibility.js";
 import { getStyledComponentsCss } from "./styledComponents.js";
 import { selectBrowsersAndVersions } from "./userSelection.js";
+import { renderResult } from "./result.js";
+import chalk from "chalk";
+import figlet from "figlet";
 
 const currentPath = process.cwd();
 
@@ -32,6 +35,35 @@ async function main() {
   const userSelections = await selectBrowsersAndVersions();
   const cssInfo = await getUserCssData();
   const result = await checkCssCompatibility(cssInfo, userSelections);
+  const resultToShow = renderResult(userSelections, result);
+
+  figlet(
+    "Check Your CSS",
+    {
+      font: "slant",
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      // 아스키 아트에 색상 적용
+      console.log(chalk.redBright.bold(data));
+      Object.values(resultToShow).forEach(notSupport => {
+        console.log(`Property: ${notSupport.property}`);
+        notSupport.lines.forEach(line => {
+          console.log(`Used At: ${line}`);
+        });
+
+        notSupport.notices.forEach(notice => {
+          console.log(`Compatibility: ${notice}`);
+        });
+
+        console.log(" ");
+      });
+    },
+  );
 }
 
 main();
